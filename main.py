c30256b75a0ce3b23ae03109a528f3e6a2e41fb5
ru55o <68447069+byru55o@users.noreply.github.com>
2022-11-24 22:49:47 +0100

    Hexadecimal with new ZWC
    ## Primary changes
    - Added all the new zero-width characters (see the README.md on the main page)
    - Changed binary system to hexadecimal to achieve shorter secrets (compression)
    
    
    ## Minor changes
    - Fixed imports (sometimes they'd cause problems because of the way there were importing)

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 #!/usr/bin/env python3
 
-import readline
-from pyperclip import copy, PyperclipException
+import readline, pyperclip
+
+zwc = [b'\xe2\x81\xa0'.decode("utf-8"), b'\xe2\x80\x8b'.decode("utf-8"), b'\xe2\x80\x8d'.decode("utf-8"),
+       b'\xe2\x80\x8e'.decode("utf-8"), b'\xe2\x80\x8f'.decode("utf-8"), b'\xe2\x80\x8c'.decode("utf-8"),
+       b'\xe2\x81\xa1'.decode("utf-8"), b'\xe1\xa0\x8e'.decode("utf-8"), b'\xe2\x80\xaa'.decode("utf-8"),
+       b'\xe2\x80\xac'.decode("utf-8"), b'\xe2\x80\xad'.decode("utf-8"), b'\xe2\x81\xa2'.decode("utf-8"),
+       b'\xe2\x81\xa3'.decode("utf-8"), b'\xe2\x81\xa4'.decode("utf-8"), b'\xe2\x81\xa5'.decode("utf-8"),
+       b'\xe2\x81\xa6'.decode("utf-8")]
 
 
-def str2bin(st):
-    return ' '.join(format(ord(x), 'b') for x in st)
+def str2hex(st):
+    return st.encode('utf-8').hex()
 
 
-def bin2hid(bi):
-    return bi.replace('1', b'\xE2\x80\x8B'.decode("utf-8")).replace('0', b'\xE2\x81\xA0'.decode("utf-8")) \
-        .replace(' ', b'\xE2\x80\x8C'.decode("utf-8"))
+def hex2hid(he):
+    for i in range(16):
+        he = he.replace(hex(i).split('x')[-1], zwc[i])
+    return he
 
 
-def wrap(msg):
-    return b'\xEF\xBB\xBF'.decode("utf-8") + msg + b'\xEF\xBB\xBF'.decode("utf-8")
+def wrap(m):
+    return b'\xEF\xBB\xBF'.decode("utf-8") + m + b'\xEF\xBB\xBF'.decode("utf-8")
 
 
-def unwrap(msg):
+def unwrap(m):
     arr = []
-    for i in range(int((len(msg.split(b'\xEF\xBB\xBF'.decode("utf-8")))) / 2)):
-        arr.append(msg.split(b'\xEF\xBB\xBF'.decode("utf-8"))[2 * i + 1])
+    for i in range(int((len(m.split(b'\xEF\xBB\xBF'.decode("utf-8")))) / 2)):
+        arr.append(m.split(b'\xEF\xBB\xBF'.decode("utf-8"))[2 * i + 1])
     return arr
 
 
-def hid2bin(hi):
+def hid2hex(hi):
     arr = []
-    for msg in hi:
-        arr.append(msg.replace(b'\xE2\x80\x8B'.decode("utf-8"), '1').replace(b'\xE2\x81\xA0'.decode("utf-8"), '0')
-                   .replace(b'\xE2\x80\x8C'.decode("utf-8"), ' '))
+    for m in hi:
+        for i in range(16):
+            m = m.replace(zwc[i], hex(i).split('x')[-1])
+        arr.append(m)
     return arr
 
 
-def bin2str(bi):
+def hex2str(he):
     arr = []
-    for msg in bi:
-        arr.append(''.join([chr(int(x, 2)) for x in msg.split(' ')]))
+    for m in he:
+        arr.append(bytearray.fromhex(m).decode('utf-8'))
     return arr
 
 
@@ -48,22 +56,23 @@ while True:
     if ch == "1":
         secret_msg = input("Enter secret message: ")
         init_str = input("Enter text where the message should be hidden: ")
-        result = init_str[:(int(len(init_str) / 2))] + wrap(bin2hid(str2bin(secret_msg))) + \
+        result = init_str[:(int(len(init_str) / 2))] + wrap(hex2hid(str2hex(secret_msg))) + \
             init_str[int(len(init_str) / 2):]
         print("[Hidden Message inside initial string]: " + result)
         try:
-            copy(result)
+            pyperclip.copy(result)
             print("The result has been copied to clipboard!")
-        except PyperclipException as e:
+        except pyperclip.PyperclipException as e:
             print(e)
             print("The result couldn't be copied to clipboard :(, (maybe) missing xclip")
         print("_" * 50)
     elif ch == "2":
         result = input("Enter text with hidden message: ")
         print("\n[Secret message(s) revealed]:\n")
-        for msg in bin2str(hid2bin(unwrap(result))):
+        for msg in hex2str(hid2hex(unwrap(result))):
             print("    " + msg)
             print("_" * 50)
     elif ch == "0":
         print("Goodbye!")
         quit()
+
